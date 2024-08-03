0c604d0c3d7a5f18cd5b809e691355228d221a54
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-08-03 03:15:49 +0300

    upload

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,9 +20,19 @@ export default function RootLayout({ children }) {
         <link rel="icon" href="/WhatsApp Image 2024-07-11 at 21.01.51_df437c70.png" sizes="52x52" />
 
         <meta name="google-site-verification" content="-1HDfMA8r2MPfNp6oa5PTR1Pe2Z-g4CThr_hEn1rIsM" />
-        
+
         </head>
       <body className={inter.className}>     
+
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5L4WGBVJ3F"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5L4WGBVJ3F');
+</script>
+
       <StoreProvider>
       <NextUIProvider>
       {children}
