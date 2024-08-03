186236e4819c1632c54f13d10210e278157be360
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-08-03 03:40:52 +0300

    fixe

--- src/app/componant/GoogleAnalytics.js
+++ src/app/componant/GoogleAnalytics.js
@@ -1,24 +1,25 @@
-// components/GoogleAnalytics.js
 import Script from 'next/script';
 
-function GoogleAnalytics() {
-  const GA_TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
-
+function GoogleAnalytics({ trackingId }) {
   return (
     <>
-      <Script
-        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
-        strategy="afterInteractive"
-      />
-      <Script id="google-analytics" strategy="afterInteractive">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
+      {trackingId && (
+        <>
+          <Script
+            src={`https://www.googletagmanager.com/gtag/js?id=${trackingId}`}
+            strategy="afterInteractive"
+          />
+          <Script id="google-analytics" strategy="afterInteractive">
+            {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
 
-          gtag('config', '${GA_TRACKING_ID}');
-        `}
-      </Script>
+              gtag('config', '${trackingId}');
+            `}
+          </Script>
+        </>
+      )}
     </>
   );
 }
