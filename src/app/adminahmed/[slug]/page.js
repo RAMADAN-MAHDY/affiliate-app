3eadb8ef27a3d56d2a664e10f3147b724eccf11e
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-08-04 22:43:46 +0300

    update

--- src/app/adminahmed/[slug]/page.js
+++ src/app/adminahmed/[slug]/page.js
@@ -223,7 +223,7 @@ const cancelEdit = () => {
 <div className='flex  justify-around items-center font-bold text-[#000]' >
 
     <p> {data[0].name}</p>   
-     
+
     <p> {data[0].code}</p>  
 
 </div>
@@ -322,7 +322,7 @@ setshowMessage(true)
                 <select className='rounded-2xl p-1 '
                     id={`options-${rowData._id}`}
                     value={selectedOptions[rowData._id] || ''}
-                    onChange={(event) => handleOptionChange(event, rowData._id, data.code)}
+                    onChange={(event) => handleOptionChange(event, rowData._id, data[0].code)}
                 >
                     {/* <option value="">اختر...</option> */}
                     <option value="0"> قيد المراجعه</option>
@@ -335,12 +335,12 @@ setshowMessage(true)
                 {
                     selectedOptions[rowData._id] === '(خصم العموله)ملغي' &&
                     <input className='p-2 rounded-2xl' type='text' placeholder='ملغي ' value={sendreqIsfiled[rowData._id] || ''} onChange={
-                        (event) => handleInputChange(event, rowData._id, data.code)}
+                        (event) => handleInputChange(event, rowData._id, data[0].code)}
                     />
 
                 }
 
-                <button className=' bg-[#12e512df] p-2 rounded-2xl m-1' onClick={() => handleUpdateStatus(rowData._id, data.code)}>تحديث الحالة</button>
+                <button className=' bg-[#12e512df] p-2 rounded-2xl m-1' onClick={() => handleUpdateStatus(rowData._id, data[0].code)}>تحديث الحالة</button>
             </div>
         </td>
         <td className="border border-gray-800 px-4 py-2" style={{ whiteSpace: 'nowrap' }}>{format(rowData.timestamp, `dd-MM & hh:mm a`)}</td>
