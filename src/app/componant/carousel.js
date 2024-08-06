23d93adc4b208e6f4dec87dfe3c5280c1d633572
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-08-06 10:09:34 +0300

    عح

--- src/app/componant/carousel.js
+++ src/app/componant/carousel.js
@@ -51,15 +51,15 @@ const CarouselFadeExample = () => {
     <div className="relative w-full md:h-[800px] sm:mt-[0px] overflow-hidden">
       <div className="relative w-full h-[50vh] md:h-[100vh] lg:h-[700vh]">
         {images.map((image, index) => (
-          <div key={index} className={`absolute inset-0 transition-opacity duration-1000 ${index === currentSlide ? 'opacity-100' : 'opacity-0'} bg-gray-900 bg-opacity-50 w-full h-[50vh] md:h-[60vh] lg:h-[70vh]`}>
+          <div key={index} className={`absolute inset-0 transition-opacity duration-1000 ${index === currentSlide ? 'opacity-100' : 'opacity-0'}  w-full h-[50vh] md:h-[60vh] lg:h-[70vh]`}>
             <img
             
               src={image.src}
               alt={image.label}
-              className="w-full md:mx-[20%]  h-full md:h-[60vh] lg:h-[70vh] object-cover img-responsive"
+              className="w-full md:w-3/5 lg:w-1/2 h-full object-cover img-responsive"
               loading="lazy"
             />
-            <div className="absolute bottom-0 left-0 right-0 bg-black bg-opacity-50 p-4">
+            <div className="absolute bottom-0 left-0 right-0  p-4">
               <h3 className="text-white text-lg">{image.label}</h3>
               <p className="text-white text-sm">{image.caption}</p>
             </div>
