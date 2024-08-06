c82014f8bf06f37284add01968dcdbd0c09cb8b3
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-08-06 08:42:41 +0300

    change category button

--- src/app/componant/catgory.jsx
+++ src/app/componant/catgory.jsx
@@ -2,57 +2,91 @@ import React, { useState } from "react";
 import { Dropdown, DropdownTrigger, DropdownMenu, DropdownItem, Button } from "@nextui-org/react";
 
 const DropdownCategory = ({ getCategory }) => {
-  const [buttonText, setButtonText] = useState("اختر فئة");
+    //   const [buttonText, setButtonText] = useState("اختر فئة");
 
-  const handleAction = (key, value) => {
-    setButtonText(value); // تحديث النص في الزر
-    getCategory(key); // تمرير المفتاح إلى الدالة المرسلة
-  };
+    const handleAction = (key, value) => {
+        // setButtonText(value); // تحديث النص في الزر
+        getCategory(key); // تمرير المفتاح إلى الدالة المرسلة
+    };
 
-  return (
-    <Dropdown>
-      <DropdownTrigger>
-        <Button variant="faded">{buttonText}</Button>
-      </DropdownTrigger>
-      <DropdownMenu aria-label="Action event example">
-        <DropdownItem
-          key="products/5"
-          textValue=" ستائر ومفروشات"
-          onClick={() => handleAction("products/5", " ستائر ومفروشات")}
-        >
-           ستائر و مفروشات 
-        </DropdownItem>
-        <DropdownItem
-          key="products/1"
-          textValue=" منوعات"
-          onClick={() => handleAction("products/1", " منوعات")}
-        >
-           منوعات
-        </DropdownItem>
-        <DropdownItem
-          key="products/2"
-          textValue="ملابس"
-          onClick={() => handleAction("products/2", "ملابس")}
-        >
-          ملابس
-        </DropdownItem>
-        <DropdownItem
-          key="products/3"
-          textValue="منتجات منزلية"
-          onClick={() => handleAction("products/3", "منتجات منزلية")}
-        >
-          منتجات منزلية
-        </DropdownItem>
-        <DropdownItem
-          key="products/4"
-          textValue="منتجات اوت دور (حدائق)"
-          onClick={() => handleAction("products/4", "منتجات اوت دور (حدائق)")}
-        >
-          منتجات اوت دور (حدائق)
-        </DropdownItem>
-      </DropdownMenu>
-    </Dropdown>
-  );
+    return (
+        // <Dropdown>
+        //   <DropdownTrigger>
+        //     <Button variant="faded">{buttonText}</Button>
+        //   </DropdownTrigger>
+        //   <DropdownMenu aria-label="Action event example">
+        //     <DropdownItem
+        //       key="products/5"
+        //       textValue=" ستائر ومفروشات"
+        //       onClick={() => handleAction("products/5", " ستائر ومفروشات")}
+        //     >
+        //        ستائر و مفروشات 
+        //     </DropdownItem>
+        //     <DropdownItem
+        //       key="products/1"
+        //       textValue=" منوعات"
+        //       onClick={() => handleAction("products/1", " منوعات")}
+        //     >
+        //        منوعات
+        //     </DropdownItem>
+        //     <DropdownItem
+        //       key="products/2"
+        //       textValue="ملابس"
+        //       onClick={() => handleAction("products/2", "ملابس")}
+        //     >
+        //       ملابس
+        //     </DropdownItem>
+        //     <DropdownItem
+        //       key="products/3"
+        //       textValue="منتجات منزلية"
+        //       onClick={() => handleAction("products/3", "منتجات منزلية")}
+        //     >
+        //       منتجات منزلية
+        //     </DropdownItem>
+        //     <DropdownItem
+        //       key="products/4"
+        //       textValue="منتجات اوت دور (حدائق)"
+        //       onClick={() => handleAction("products/4", "منتجات اوت دور (حدائق)")}
+        //     >
+        //       منتجات اوت دور (حدائق)
+        //     </DropdownItem>
+        //   </DropdownMenu>
+        // </Dropdown>
+
+        <div className="flex flex-wrap gap-6 items-center">
+
+            <Button color="success"
+                onClick={() => handleAction("products/3")}
+            >
+                منتجات منزلية
+            </Button>
+            
+            <Button color="primary"
+                onClick={() => handleAction("products/1")}
+            >
+                منوعات
+            </Button>
+
+            <Button color="secondary"
+                onClick={() => handleAction("products/2", "ملابس")}
+            >
+                ملابس
+            </Button>
+
+            <Button color="default" onClick={() => handleAction("products/5")}>
+                ستائر ومفروشات
+            </Button>
+
+            <Button color="warning"
+                onClick={() => handleAction("products/4")}
+            >
+                منتجات اوت دور (حدائق)
+            </Button>
+
+        </div>
+
+
+    );
 };
 
 export default DropdownCategory;
