7905a97f8ac04223d4998c68f81759b532ed36c5
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2024-08-07 22:13:42 +0300

    update

--- src/app/componant/catgory.jsx
+++ src/app/componant/catgory.jsx
@@ -1,67 +1,66 @@
 "use client";
 import React, { useState } from "react";
-import { Button } from "@nextui-org/react";
 import style from "./cssSheet/marquee.module.css"; // تأكد من إضافة ملف CSS هذا
 
 const DropdownCategory = ({ getCategory }) => {
-    const [buttonFocas, setButtonfocas] = useState(null);
+    const [buttonFocas, setbuttonfocas] = useState(null);
 
     const handleAction = (key, value) => {
-        setButtonfocas(value); // تحديث النص في الزر
+        setbuttonfocas(value); // تحديث النص في الزر
         getCategory(key); // تمرير المفتاح إلى الدالة المرسلة
     };
 
     return (
         <div className="mb-3 overflow-x-auto flex space-x-3">
-            <Button
+            <button
                 color="secondary"
-                className={`${buttonFocas==="1" ?  style.buttonActive : "p-3"} w-40 h-12`}
+                className={`${buttonFocas==="1" ?  style.buttonActive : "p-3"} w-40 h-12 bg-[#bd02ec] rounded-3xl `}
                 onClick={() => handleAction("products/3", "1")}
             >
-                <div className={style.marquee}>
-                    <span>منتجات منزلية</span>
+                <div className={`${buttonFocas==="1" && style.marquee} relative overflow-hidden whitespace-nowrap`}>
+                    <span className="inline-block">منتجات منزلية</span>
                 </div>
-            </Button>
+            </button>
             
-            <Button
+            <button
                 color="secondary"
-                className={`${buttonFocas==="2" ?  style.buttonActive : "p-1"} w-[170px] h-12`}
+                className={`${buttonFocas==="2" ?  style.buttonActive : "p-1"} w-[460px] h-12 bg-[#bd02ec] rounded-3xl`}
                 onClick={() => handleAction("products/1", '2')}
             >
-                <div className={style.marquee}>
-                    <span>اجهزه الكترونيه ( ساعات - سماعات - موبيلات )</span>
+                <div className={`${buttonFocas === "2" && style.marquee} relative overflow-hidden whitespace-nowrap`}>
+                    <span className="inline-block">اجهزه الكترونيه ( ساعات - سماعات - موبيلات )</span>
                 </div>
-            </Button>
+            </button>
 
-            <Button
+            <button
                 color="secondary"
-                className={`${buttonFocas==="3" ?  style.buttonActive : "p-1"} w-40 h-12`}
+                className={`${buttonFocas==="3" ?  style.buttonActive : "p-1"} w-[220px] h-12 bg-[#bd02ec] rounded-3xl`}
                 onClick={() => handleAction("products/2", "3")}
             >
-                <div className={style.marquee}>
-                    <span>ملابس ( رجالي - حريمي - اطفال )</span>
+                <div className={`${buttonFocas==="3" && style.marquee} relative overflow-hidden whitespace-nowrap`}>
+                    <span className="inline-block">ملابس ( رجالي - حريمي - اطفال )</span>
                 </div>
-            </Button>
+            </button>
 
-            <Button
+            <button
                 color="secondary"
-                className={`${buttonFocas==="4" ?  style.buttonActive : "p-1"} w-40 h-12`}
+                className={`${buttonFocas==="4" ?  style.buttonActive : "p-1"} w-[175px] h-12 bg-[#bd02ec] rounded-3xl`}
                 onClick={() => handleAction("products/5", '4')}
             >
-                <div className={style.marquee}>
-                    <span>ستائر ومفروشات</span>
+                <div className={`${buttonFocas==="4" && style.marquee} relative overflow-hidden whitespace-nowrap`}>
+                    <span className="inline-block">ستائر ومفروشات</span>
                 </div>
-            </Button>
+            </button>
 
-            <Button
+            <button
                 color="secondary"
-                className={`${buttonFocas==="5" ? style.buttonActive : "p-1"} w-40 h-12`}
+                className={`${buttonFocas==="5" ? style.buttonActive : "p-1"} w-40 h-12 bg-[#bd02ec] rounded-3xl`}
                 onClick={() => handleAction("products/4", '5')}
             >
-                <div className={style.marquee}>
-                    <span>منتجات اوت دور ( حدائق )</span>
+                <div className={`${buttonFocas==="5" && style.marquee} relative overflow-hidden whitespace-nowrap`}>
+                    <span className="inline-block">منتجات اوت دور ( حدائق )</span>
                 </div>
-            </Button>
+            </button>
         </div>
     );
 };
