1244cece8e2f5ad64e3c0e43739ef48b27fbfcf0
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-04-26 02:10:38 +0300

    add security headers to next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '**',
-          },
-        ],
-      },
-
-
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: '**',
+        },
+      ],
+    },
+  
     productionBrowserSourceMaps: false,
+  
     webpack: (config, { isServer }) => {
       if (isServer) {
         config.resolve.alias['punycode'] = 'punycode/';
@@ -18,7 +18,50 @@ const nextConfig = {
       return config;
     },
   
-};
-
-export default nextConfig;
-
+    async headers() {
+      const securityHeaders = [
+        {
+          key: "Content-Security-Policy",
+          value: `
+            default-src 'self';
+            script-src 'self' 'unsafe-inline' 'unsafe-eval';
+            style-src 'self' 'unsafe-inline';
+            img-src 'self' data:;
+            font-src 'self';
+            connect-src 'self';
+            frame-ancestors 'self';
+          `.replace(/\n/g, ""),
+        },
+        {
+          key: "X-Frame-Options",
+          value: "SAMEORIGIN",
+        },
+        {
+          key: "X-Content-Type-Options",
+          value: "nosniff",
+        },
+        {
+          key: "Referrer-Policy",
+          value: "strict-origin-when-cross-origin",
+        },
+        {
+          key: "Permissions-Policy",
+          value: "camera=(), microphone=(), geolocation=()",
+        },
+        {
+          key: "Strict-Transport-Security",
+          value: "max-age=63072000; includeSubDomains; preload",
+        },
+      ];
+  
+      return [
+        {
+          source: "/(.*)",
+          headers: securityHeaders,
+        },
+      ];
+    },
+  };
+  
+  export default nextConfig;
+  
