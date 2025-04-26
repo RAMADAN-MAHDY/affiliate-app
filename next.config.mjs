ec7ef3961b2dcec8b127acabff88ebc576e7bff2
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-04-26 19:39:08 +0300

    update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,27 +4,6 @@ const nextConfig = {
     experimental: {
       serverActions: true,
     },
-    headers: async () => {
-      return [
-        {
-          source: '/(.*)', // Apply to all pages
-          headers: [
-            {
-              key: 'Content-Security-Policy',
-              value: `
-                default-src 'self';
-                script-src 'self' https://www.googletagmanager.com;
-                connect-src 'self' https://www.google-analytics.com;
-                img-src 'self' data: https://www.google-analytics.com;
-                style-src 'self' 'unsafe-inline';
-                font-src 'self' https://fonts.googleapis.com https://fonts.gstatic.com;
-                frame-src https://www.googletagmanager.com;
-              `.replace(/\s{2,}/g, ' ').trim(),
-            },
-          ],
-        },
-      ];
-    },
   };
   
   export default nextConfig;
