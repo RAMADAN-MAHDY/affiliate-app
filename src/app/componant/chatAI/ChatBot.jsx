5b2deba96d11358fd9d20cbabff654f1ec0f2880
RAMADAN-MAHDY <104697578+RAMADAN-MAHDY@users.noreply.github.com>
2025-09-23 05:52:33 +0300

    add my fone

--- src/app/componant/chatAI/ChatBot.jsx
+++ src/app/componant/chatAI/ChatBot.jsx
@@ -89,7 +89,7 @@ export default function ChatBotWidget() {
             if (!data.token && token) {
                 localStorage.removeItem("token");
             }
-            setHistory(h => [...h, { from: "bot", text: data?.reply || " يوجد ضغط حاليا من فضلك التواصل علي هذا الرقم 0562790402 او حاول ثانيا" }]);
+            setHistory(h => [...h, { from: "bot", text: data?.reply || " يوجد ضغط حاليا من فضلك التواصل علي هذا الرقم 01021256768 او حاول ثانيا" }]);
         } catch {
             setHistory(h => [...h, { from: "bot", text: "صارت مشكلة في الاتصال بالمساعد. يرجي التواصل عبر الجوال 01021256768" }]);
         }
